32926126715ed89665f8827f498e8a02ca81acf7
wdj3488 <wdj1218@126.com>
2020-08-16 20:57:03 +0800

    设置报警输出的等级

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 print(tf.test.is_gpu_available())
 a = tf.constant(2.)
@@ -6,4 +8,4 @@ b = tf.constant(4.)
 print(a*b)
 
 # import numpy as np
-# print(np.array([0, 10]))
+# print(np.arange(0, 10))
