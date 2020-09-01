524f1ed9c49bf3233c45908c7e7fe882cff09811
wdj3488 <wdj1218@126.com>
2020-09-01 11:13:02 +0800

    meshgrid的实践代码

---
+++ meshgrid.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def func(x):
+    z = tf.math.sin(x[..., 0]) + tf.math.cos(x[..., 1])
+    return z
+
+
+x = tf.linspace(0., 2*3.14, 500)
+y = tf.linspace(0., 2*3.14, 500)
+point_x, point_y = tf.meshgrid(x, y)
+points = tf.stack([point_x, point_y], axis=2)
+print('points:', points.shape)
+z = func(points)
+print('z:', z.shape)
+# plt.imshow(z, origin='lower', interpolation='none')
+plt.contour(point_x, point_y, z)
+
+plt.show()
